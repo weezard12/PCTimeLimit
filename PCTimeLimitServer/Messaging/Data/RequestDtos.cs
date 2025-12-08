6428e0b1987e0b06b2b52834c7f4e215a934595a
weezard12 <weezard1234@gmail.com>
2025-12-08 16:23:30 +0200

    removed duplicate classes

--- PCTimeLimitServer/Messaging/Data/RequestDtos.cs
+++ PCTimeLimitServer/Messaging/Data/RequestDtos.cs
@@ -2,18 +2,6 @@ using System;
 
 namespace PCTimeLimitServer;
 
-public class CreateAccountData
-{
-    public string Username { get; set; } = "";
-    public string Password { get; set; } = "";
-    public bool IsAdmin { get; set; } = false;
-}
-
-public class LoginData
-{
-    public string Username { get; set; } = "";
-    public string Password { get; set; } = "";
-}
 
 public class RegisterComputerData
 {
@@ -21,19 +9,6 @@ public class RegisterComputerData
     public string ComputerName { get; set; } = "";
     public string AdminUsername { get; set; } = "";
     public string? AdminCode { get; set; }
-}
-
-public class UpdateComputerStatusData
-{
-    public string ComputerId { get; set; } = "";
-    public bool IsOnline { get; set; } = false;
-}
-
-public class SetComputerTimeLimitData
-{
-    public string ComputerId { get; set; } = "";
-    public TimeSpan DailyTimeLimit { get; set; } = TimeSpan.FromHours(1);
-    public string AdminUsername { get; set; } = "";
 }
 
 public class GetComputersForAdminData
